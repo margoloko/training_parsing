b48e9c7ef83e986bdf39fd1d54e271f5bc18213d
1 <margoloko@mail.ru>
2023-02-09 13:58:18 +0700

    initial

---
+++ caching.py
@@ -0,0 +1,12 @@
+import requests_cache
+
+
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+
+if __name__ == '__main__':
+    
+    session = requests_cache.CachedSession()
+
+    
+    response = session.get(MAIN_DOC_URL)
+    print(list(session.cache.urls))
