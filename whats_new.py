f79b7510a1db04cbfd5ff735dd4d11aa41180c23
1 <margoloko@mail.ru>
2023-02-14 10:35:29 +0700

    cashedsession

--- whats_new.py
+++ whats_new.py
@@ -1,6 +1,7 @@
-import requests
-from requests_cache import CashedSession
+#import requests
+import requests_cache
 
-URL = https://docs.python.org/3/whatsnew/
+URL = 'https://docs.python.org/3/whatsnew/'
 
-session = CashedSession()
+session = requests_cache.CashedSession()
+print(session.get(URL))
